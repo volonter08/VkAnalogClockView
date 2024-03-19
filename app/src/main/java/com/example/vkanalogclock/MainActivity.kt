7a4f88c3573b9b7513aca46af487bbb34e2a0e4a
Oleg Badmaev <bosyou4@gmail.com>
2024-03-19 23:45:03 +0300

    Release commit

---
+++ app/src/main/java/com/example/vkanalogclock/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.vkanalogclock
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.vkanalogclock.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        binding.clockView.startClock()
+        super.onStart()
+    }
+}
